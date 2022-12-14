566632081afcff32695819305042bb234caa32f8
katp98 <kethzyp@gmail.com>
2022-12-14 11:29:53 +0530

    mongoDB - typeorm cookie-based: COMPLETED
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- TypeORM-database-template/MongoDB/database/models/user.model.ts
+++ TypeORM-database-template/MongoDB/database/models/user.model.ts
@@ -1,19 +1,20 @@
-import { Entity, ObjectIdColumn, Column, BaseEntity } from 'typeorm';
-import { ObjectId } from 'mongodb';
+import { Entity, ObjectIdColumn, Column, BaseEntity } from "typeorm";
+import { ObjectId } from "mongodb";
 
 @Entity()
 export class User extends BaseEntity {
-  @ObjectIdColumn()
-    _id!: ObjectId;
+  @ObjectIdColumn({ primary: true})
+  _id!: ObjectId;
 
   @Column()
-    name!: string;
+  name!: string;
 
   @Column()
-    email!: string;
+  email!: string;
 
   @Column()
-    password!: string;
+  password!: string;
 
-
+  @Column({ nullable: true })
+  refreshToken!: string;
 }
--- TypeORM-database-template/MongoDB/database/repositories/user.repository.ts
+++ TypeORM-database-template/MongoDB/database/repositories/user.repository.ts
@@ -36,3 +36,45 @@ export const createUser = async ({
     return { message: "user exists" };
   }
 };
+
+//@desc SAVING REFRESH TOKEN
+//@route POST
+export const saveRefreshToken = async (
+  userID: string,
+  RefreshToken: string
+) => {
+  //finding the user according to ID and update:
+  await User.createQueryBuilder()
+    .update(User)
+    .set({ refreshToken: RefreshToken })
+    .where("_id = :_id", { _id: userID })
+    .execute();
+};
+
+//@desc FINDING USER BY ID
+//@route POST
+export const findUserById = async (id: string) => {
+  try {
+    const existingUser = await User.createQueryBuilder("user")
+      .where("_id = :_id", { _id: id })
+      .select(["user.password", "user.refreshToken"]);
+    console.log(existingUser);
+  } catch (error) {
+    return {
+      error: "error",
+    };
+  }
+};
+
+//@desc REMOVING REFRESH TOKEN
+//@route POST
+export const removeRefreshToken = async (RefreshToken: string) => {
+  try {
+    const user = await User.findOneBy({ refreshToken: RefreshToken });
+    if (user) {
+      user.refreshToken = "";
+      const result = await user.save();
+      console.log(result);
+    }
+  } catch (error) {}
+};
